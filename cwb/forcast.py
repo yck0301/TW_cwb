a363c92454733be69f2cfacb8ab00b6834b1fbb0
yck0301 <yck0301@gmail.com>
2015-08-27 17:42:42 +0800

    Retrieve forcast data from TW cwb, and convert xml to json

---
+++ cwb/forcast.py
@@ -0,0 +1,14 @@
+﻿#coding=UTF-8
+import requests
+import xmltodict
+import json
+import io
+
+BASE_URL = "http://opendata.cwb.gov.tw/opendataapi"
+payload = {"dataid": "F-C0032-001", "authorizationkey": ""}
+
+resp = requests.get(BASE_URL, params=payload)
+respJson = json.dumps(xmltodict.parse(resp.text), indent=4, ensure_ascii=False)
+file = io.open("cwbJson.txt", "w", encoding="utf-8")
+file.write(respJson)
+file.close()
